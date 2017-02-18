13cb4d43c3ba15dff9c31320410c8c504f44a744
Brandon Pereira <brandonp@criticalmass.com>
2017-02-18 12:23:05 -0700

    sort shows by airdate

--- src/scripts/Services/Storage.js
+++ src/scripts/Services/Storage.js
@@ -1,13 +1,27 @@
 import uniqBy from 'lodash/uniqBy';
 
 function addShow(show) {
-	var currentShows = JSON.parse(localStorage.getItem('shows')) || [];
+	var currentShows = getShows();
 	currentShows.push(show);
-	localStorage.setItem('shows', JSON.stringify(uniqBy(currentShows, 'id')));
+	setShows(sortShows(currentShows));
 }
 
 function getShows() {
-	return JSON.parse(localStorage.getItem('shows'));
+	return JSON.parse(localStorage.getItem('shows')) || [];
+}
+
+function setShows(shows) {
+	console.log(shows);
+	localStorage.setItem('shows', JSON.stringify(uniqBy(shows, 'id')));
+}
+
+function sortShows(unsortedShows) {
+	const TBA_DATE = new Date("01/01/3000");
+	return unsortedShows.sort(function(a, b) {
+    a = a.nextepisode ? new Date(a.nextepisode.airstamp) : TBA_DATE;
+    b = b.nextepisode ? new Date(b.nextepisode.airstamp) : TBA_DATE;
+    return a - b;
+	});
 }
 
 export default {addShow, getShows};
