c418fecf7b47661a14b636879403759fef39c961
Brandon Pereira <brandonp@criticalmass.com>
2017-02-22 19:54:07 -0700

    async load touch events

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,13 +3,13 @@ import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import routes from './Router/Router';
 import register from './Services/ServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+
 // MUI
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import theme from './theme';
 
-// TODO: Load Async 
-injectTapEventPlugin();
+// Touch Events
+import('react-tap-event-plugin').then(injectTapEventPlugin => injectTapEventPlugin());
 
 render((
 	<MuiThemeProvider muiTheme={theme}>
