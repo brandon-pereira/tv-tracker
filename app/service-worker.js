8f793cd72e18b18bec4989e6a4ef735cc7d207e2
Brandon Pereira <brandonp@criticalmass.com>
2017-02-23 17:44:38 -0700

    base sw

--- app/service-worker.js
+++ app/service-worker.js
@@ -3,13 +3,39 @@ var self = this;
 
 this.addEventListener('install', function(event) {
 	console.log("Installing");
-  // event.waitUntil(
-  //    caches.open(VERSION).then(function(cache) {
-  //     return cache.addAll([
-  //       '/'
-	// 		]);
-  //   })
-  // );
+  event.waitUntil(
+     caches.open(VERSION).then(function(cache) {
+      return cache.addAll([
+        '/',
+				'/style.css',
+				'/app.js',
+				'/0.app.js',
+				'/1.app.js',
+				'/2.app.js',
+				'/3.app.js',
+				'/4.app.js',
+			]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+				console.log(response);
+				// var isCatAPI = /getrandomcatpic/.test(event.request.url);
+        if (response) {
+					// Cache hit - return the response from the cached version
+          return response;
+				} else {
+					// Not in cache - return the result from the live server
+					// `fetch` is essentially a "fallback"
+					return fetch(event.request);
+				}
+      }
+    )
+  );
 });
 
 // this.addEventListener('message', function(event) {
