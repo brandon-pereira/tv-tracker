fb1a6df3e86a7100f6bf82115fa7a8e256ca4aff
Brandon Pereira <brandonp@criticalmass.com>
2017-03-04 21:08:13 -0700

    removed caching temporarily

--- app/service-worker.js
+++ app/service-worker.js
@@ -6,34 +6,33 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
      caches.open(VERSION).then(function(cache) {
       return cache.addAll([
-        '/',
-				'/style.css',
-				'/app.js',
-				'/0.app.js',
-				'/1.app.js',
-				'/2.app.js',
-				'/3.app.js',
-				'/4.app.js',
+        // '/',
+				// '/style.css',
+				// '/app.js',
+				// '/0.app.js',
+				// '/1.app.js',
+				// '/2.app.js',
+				// '/3.app.js',
+				// '/4.app.js',
 			]);
     })
   );
 });
 
-this.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-				console.log(response);
-				// var isCatAPI = /getrandomcatpic/.test(event.request.url);
-        if (response) {
-					// Cache hit - return the response from the cached version
-          return response;
-				} else {
-					// Not in cache - return the result from the live server
-					// `fetch` is essentially a "fallback"
-					return fetch(event.request);
-				}
-      }
-    )
-  );
-});
+// this.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+// 				// var isCatAPI = /getrandomcatpic/.test(event.request.url);
+//         if (response) {
+// 					// Cache hit - return the response from the cached version
+//           return response;
+// 				} else {
+// 					// Not in cache - return the result from the live server
+// 					// `fetch` is essentially a "fallback"
+// 					return fetch(event.request);
+// 				}
+//       }
+//     )
+//   );
+// });
