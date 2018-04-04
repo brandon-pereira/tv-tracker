5c44cbea3258df3f9d523563cd3051977ac56bf0
Brandon Pereira <brandonp@criticalmass.com>
2018-04-04 16:09:45 -0600

    started thinking about checking for new episodes in a queue

--- server/scheduler.js
+++ server/scheduler.js
@@ -3,11 +3,13 @@ const pushnotifications = require('./pushnotifications');
 const _get = require('lodash.get');
 
 module.exports = (db) => {
-    const rule = new schedule.RecurrenceRule();
-    rule.second = 5;
-    schedule.scheduleJob(rule, () => checkAndSendNotifications(db));
+    schedule.scheduleJob({second: 0}, () => checkAndSendNotifications(db));
+    schedule.scheduleJob({minute: 1}, () => checkForNewEpisodes(db));
 };
 
+const checkForNewEpisodes = async (db) => {
+    console.log("checking for new episodes");
+}
 
 const checkAndSendNotifications = async (db) => {
     const episodes = await getAiredEpisodes(db, new Date());
@@ -22,7 +24,7 @@ const getAiredEpisodes = async (db, date) => {
     const aired = await db.Schedule.find({ airDate: { $lt: date }});
     const episodes = await Promise.all(aired.map(async e => {
         e = e.toObject();
-        const show = await db.TvShow.findOne({_id: e.id})
+        const show = await db.TvShow.findOne({_id: e.show_id})
         e.TvShow = show;
         return e;
     }));
@@ -34,9 +36,8 @@ const sendNotification = async (database, episode) => {
         return Promise.all(episode.TvShow.subscribedUsers.map(async _id => {
             const user = await database.Users.findOne({ _id });
             return await pushnotifications({
-                 // Silicon Valley S01E11 will air now!
-                title: `${episode.TvShow.name} S${('0' + episode.season).slice(-2)}E${('0' + episode.episode).slice(-2)} will air now!`,
-                body: episode.description || undefined,
+                title: `${episode.TvShow.name} will air now!`,
+                body: `S${('0' + episode.season).slice(-2)}E${('0' + episode.episode).slice(-2)}${episode.name ? ' - ' + episode.name : ''}` || undefined,
                 image: _get(episode, `TvShow.image.medium`, undefined)
             }, user.pushSubscription)
         }))
